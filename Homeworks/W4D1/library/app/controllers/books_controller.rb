bfe2a10ba2beb856d4e966e5ba350ccf26a26c5d
Michael Loschiavo <losch103@gmail.com>
2016-10-02 14:07:59 -0700

    Completed homework

--- Homeworks/W4D1/library/app/controllers/books_controller.rb
+++ Homeworks/W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,25 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
   end
 
   def new
-    # your code here
+
   end
 
   def create
-    # your code here
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_url
+    else
+      render "new"
+    end
   end
 
   def destroy
-    # your code here
+    @book = Book.find(params[:id])
+    @book.destroy!
+    redirect_to books_url
   end
 
   private
