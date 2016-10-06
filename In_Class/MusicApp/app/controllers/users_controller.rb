80a7c646a3a07b037d51cccfd2307633aa583bc7
Michael Loschiavo <losch103@gmail.com>
2016-10-06 13:58:28 -0700

    Auth completed. Created views for user and session. Added form partial for user#new and session#new

--- In_Class/MusicApp/app/controllers/users_controller.rb
+++ In_Class/MusicApp/app/controllers/users_controller.rb
@@ -1,17 +1,28 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(user_params)
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user
+      login!(@user)
+      redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
--- In_Class/MusicApp/app/models/user.rb
+++ In_Class/MusicApp/app/models/user.rb
@@ -1,23 +1,31 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6, allow_nil: true}
+  validates :session_token, presence: true
   validates :password_digest, presence: {message: "Password can't be blank"}
 
   attr_reader :password
-  before_initialize :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.generate_session_token
-    self.session_token = SecureRandom::url_safe64(16)
+    SecureRandom::urlsafe_base64(16)
   end
 
-  def self.ensure_session_token
-    self.session_token ||= generate_session_token
+  def self.find_by_credentials(email,password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
-  def self.reset_session_token!
+
+  def reset_session_token!
     self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
   def password=(password)
@@ -26,12 +34,9 @@ class User < ApplicationRecord
   end
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  private
 
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
+
 end
