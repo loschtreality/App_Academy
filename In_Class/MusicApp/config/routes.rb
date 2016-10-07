8ddf880613ba9356792b636c88f2565d25109b4c
Michael Loschiavo <losch103@gmail.com>
2016-10-06 20:36:57 -0700

    Began views and controller routes for Albums

--- In_Class/MusicApp/config/routes.rb
+++ In_Class/MusicApp/config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 
-  resources :bands
-  resources :albums, except: [:index]
-  resources :tracks, except: [:index]
+  resources :bands do
+    resources :albums, except: [:index] do
+      resources :tracks, except: [:index]
+      
+    end
+
+  end
 
 end
--- In_Class/MusicApp/db/schema.rb
+++ In_Class/MusicApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006233320) do
+ActiveRecord::Schema.define(version: 20161007004835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161006233320) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.string   "produced",   default: "studio"
+    t.integer  "year"
   end
 
   create_table "bands", force: :cascade do |t|
