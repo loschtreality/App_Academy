b8914de6402d5060e9d6474d6255430b8f96a601
Michael Loschiavo <losch103@gmail.com>
2016-10-22 15:11:51 -0700

    Added css and completed functionality of calculator

--- Homeworks/W6D5/react_calculator/calculator.jsx
+++ Homeworks/W6D5/react_calculator/calculator.jsx
@@ -1,13 +1,75 @@
 import React from 'react';
 
-const Calculator = React.createClass({
+class Calculator extends React.Component {
+  constructor() {
+    super()
+    this.state = {X: "", Y: "", total: 0}
+    this.updateX = this.updateX.bind(this)
+    this.updateY = this.updateY.bind(this)
+    this.add = this.add.bind(this)
+    this.subtract = this.subtract.bind(this)
+    this.multiply = this.multiply.bind(this)
+    this.divide = this.divide.bind(this)
+    this.divide = this.divide.bind(this)
+    this.clearInputs = this.clearInputs.bind(this)
+  }
+
+
+  updateX(e) {
+    this.setState({X: e.target.value})
+  }
+
+  updateY(e) {
+    this.setState({Y: e.target.value})
+  }
+
+  clearInputs(e) {
+    e.preventDefault()
+    this.setState({X: "", Y: "", total: 0})
+  }
+
+  add(e) {
+    e.preventDefault()
+    this.setState({total: parseInt(this.state.X) + parseInt(this.state.Y)})
+  }
+
+  subtract(e) {
+    e.preventDefault()
+    this.setState({total: parseInt(this.state.X) - parseInt(this.state.Y)})
+  }
+
+  multiply(e) {
+    e.preventDefault()
+    this.setState({total: parseInt(this.state.X) * parseInt(this.state.Y)})
+  }
+
+  divide(e) {
+    e.preventDefault()
+    this.setState({total: parseInt(this.state.X) / parseInt(this.state.Y)})
+  }
+
   render () {
+
     return (
       <div>
         <h1>Calculator</h1>
+        <label>X:
+          <input id="X" type="text" onChange={ this.updateX } value={this.state.X}></input>
+        </label>
+        <label>Y:
+          <input id="Y" type="text" onChange={ this.updateY } value={this.state.Y}></input>
+        </label>
+        <span className="total">{this.state.total}</span>
+
+        <hr></hr>
+        <button onClick={ this.add }>+</button>
+        <button onClick={ this.subtract }>-</button>
+        <button onClick={ this.multiply }>*</button>
+        <button onClick={ this.divide }>/</button>
+        <button onClick={ this.clearInputs }>CLEAR</button>
       </div>
     )
   }
-})
+}
 
 export default Calculator
