e71ae942d91e03914b5fd0afa74a5d2c2077684b
Michael Loschiavo <losch103@gmail.com>
2016-10-21 20:12:47 -0700

    homework phase 1

---
+++ Homeworks/W6D5/react_calculator/calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Calculator = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Calculator</h1>
+      </div>
+    )
+  }
+})
+
+export default Calculator
--- Homeworks/W6D5/react_calculator/entry.jsx
+++ Homeworks/W6D5/react_calculator/entry.jsx
@@ -1,1 +1,8 @@
-console.log("Suh");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './calculator';
+
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById("root");
+	ReactDOM.render(<Calculator/>, root);
+});
